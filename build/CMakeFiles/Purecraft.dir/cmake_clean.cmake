8fcb704f0cfb84410e41805d5c8057f659f519bd
Garzzza7 <piotr.czapela@gmail.com>
2023-07-25 11:25:48 +0200

    first

---
+++ build/CMakeFiles/Purecraft.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Purecraft.dir/src/glad.c.o"
+  "CMakeFiles/Purecraft.dir/src/glad.c.o.d"
+  "CMakeFiles/Purecraft.dir/src/main.cpp.o"
+  "CMakeFiles/Purecraft.dir/src/main.cpp.o.d"
+  "Purecraft"
+  "Purecraft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Purecraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Purecraft.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Purecraft.
+# This may be replaced when dependencies are built.
